3054f70369ac96993b219b6df5492bcc7bfa9095
Tadashi-95 <hamster460@gmail.com>
2024-08-09 14:55:07 +0900

    オブジェクトをファイルごとに分割

--- src/Board.js
+++ src/Board.js
@@ -1,49 +1,7 @@
 import { useState } from 'react'
+import Square from "./Square.js";
 
-export default function Game() {
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    const [currentMove, setCurrentMove] = useState(0);
-    const xIsNext = currentMove % 2 === 0;
-    const currentSquares = history[currentMove];
-
-    function handlePlay(nextSquares) {
-        const nextHistoty = [...history.slice(0, currentMove + 1), nextSquares];
-        setHistory(nextHistoty);
-        setCurrentMove(nextHistoty.length - 1);
-    }
-    
-    function jumpTo(nextMove) {
-        setCurrentMove(nextMove);
-    }
-    
-    const moves = history.map((squares, move) => {
-        let description;
-        if (move > 0) {
-            description = 'Go to move #' + move;
-        } else {
-            description = 'Go to game start';
-        }
-
-        return (
-            <li key={move}>
-                <button onClick={() => jumpTo(move)}>{description}</button>
-            </li>
-        );
-    });
-
-    return (
-        <div className="game">
-            <div className="game-board">
-                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
-            </div>
-            <div className="game-info">
-                <ol>{moves}</ol>
-            </div>
-        </div>
-    );
-}
-
-function Board({xIsNext, squares, onPlay}) {
+export default function Board({xIsNext, squares, onPlay}) {
     function handleClick(i) {
         if (squares[i] || calculateWinner(squares)) {
             return ;
@@ -87,14 +45,6 @@ function Board({xIsNext, squares, onPlay}) {
         </div>
     </>
   );
-}
-
-function Square({ value , onSquareClick}) {
-    return (
-        <button className="square" onClick={onSquareClick}>
-            {value}
-        </button>
-    );
 }
 
 function calculateWinner(squares) {
