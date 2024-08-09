b950a1ab8a6b1261ad2c2bf551e967bee74f0d9a
Tadashi-95 <hamster460@gmail.com>
2024-08-09 13:27:21 +0900

    day 4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { useState } from 'react'
 
 export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState(0);
-    const currentSquares = history[history.length - 1];
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
 
     function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
+        const nextHistoty = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistoty);
+        setCurrentMove(nextHistoty.length - 1);
     }
     
     function jumpTo(nextMove) {
         setCurrentMove(nextMove);
-        setXIsNext(nextMove % 2 ===0);
     }
     
     const moves = history.map((squares, move) => {
